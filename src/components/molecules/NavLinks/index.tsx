970de7ec56c5825f987b4158d4c7514052e4e27c
Michal Bartosik <msbartosik@gmail.com>
2021-07-01 13:19:30 +0200

    added styling for active link

--- src/components/molecules/NavLinks/index.tsx
+++ src/components/molecules/NavLinks/index.tsx
@@ -1,9 +1,12 @@
 import { HStack, Box } from '@chakra-ui/react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import { links } from 'utils/links'
 
 const NavLinks = (): JSX.Element => {
+  const { asPath } = useRouter()
+
   return (
     <Box as="nav" display={{ base: 'none', lg: 'block' }}>
       <HStack as="ul" display="flex" spacing={9} listStyleType="none">
@@ -16,7 +19,7 @@ const NavLinks = (): JSX.Element => {
             transition="color 0.2s linear"
             letterSpacing="0.125em"
             key={link.id}
-            color="white"
+            color={asPath === link.url ? 'accent' : 'white'}
             textTransform="uppercase"
           >
             <Link href={link.url}>{link.text}</Link>
